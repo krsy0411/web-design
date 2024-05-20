ec4a8dc145208ef3f01fb1e22861e534845b4e74
PANdas <krsy0411@naver.com>
2024-05-20 16:17:17 +0900

    파일 위치 이동

--- NoJS/darkTheme/script.js
+++
@@ -1,8 +0,0 @@
-// id가 checkbox인 html tag를 찾기
-const checkbox = document.getElementById("checkbox");
-// checkbox tag에 change라는 이벤트(input에서 값에 변화가 생길 시 발생하는 이벤트 추가
-// 텍스트를 입력받는 인풋이면 -> 글자 하나 입력할때마다 change event 발생하는겁니다) 
-checkbox.addEventListener("change", function() {
-    // body태그의 class이름에 "dark-mode"를 토글(넣었다가 뺐다 설정)
-    document.body.classList.toggle("dark-mode");
-})
